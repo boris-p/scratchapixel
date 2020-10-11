3ff6df8512bcdf1301647d1e92ed387f50b7b24b
boris-p <pborisp@gmail.com>
2020-10-11 15:16:45 +0300

    vector

--- src/geometry/Vector3.py
+++ src/geometry/Vector3.py
@@ -1,6 +1,28 @@
+import math
+
+
 class Vector3:
 
     def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
         self.z = z
+
+    def length(self):
+        return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+
+    # a normalized vector is a vector whose length is 1, also called a unit vector
+    def normalize(self):
+        vec_length = self.length()
+
+        if vec_length > 0:
+            inv_lev = 1 / vec_length
+            self.x = self.x * inv_lev
+            self.y = self.y * inv_lev
+            self.z = self.z * inv_lev
+
+    def dot(self):
+        pass
+
+    def cross(self):
+        pass
--- tests/geometry/test_Vector3.py
+++ tests/geometry/test_Vector3.py
@@ -1,4 +1,5 @@
 #import pytest
+import math
 
 from src.geometry.Vector3 import Vector3
 
@@ -6,3 +7,8 @@ from src.geometry.Vector3 import Vector3
 def test_create_vector():
     vec = Vector3(0, 0, 0)
     assert vec.x == 0
+
+
+def test_vector_length():
+    vec = Vector3(1, 1, 1)
+    assert math.isclose(vec.length(), 1.732,  abs_tol=1e-03)
