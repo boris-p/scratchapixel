9fe2aeb86954bcbbbd68dd378007f536e30c9fe0
boris-p <pborisp@gmail.com>
2021-02-16 22:25:57 +0200

    wip

--- src/jether_exercise/polygon.py
+++ src/jether_exercise/polygon.py
@@ -4,6 +4,12 @@ class PolygonIndexError(Exception):
         super().__init__(self.message)
 
 
+class PolygonRemoveIndexError(Exception):
+    def __init__(self, ind, len):
+        self.message = f'trying to remove element with index {ind} from a polygon with length {len}'
+        super().__init__(self.message)
+
+
 class Node:
     def __init__(self, pt):
         self.pt = pt
@@ -67,7 +73,21 @@ class Polygon:
         self.__total_points += 1
 
     def remove(self, ind):
-        self.__points.pop(ind)
+        if ind >= self.__total_points:
+            raise PolygonRemoveIndexError(ind, self.__total_points)
+        curr_node = self.__head
+        for _ in range(ind):
+            curr_node = curr_node.next
+
+        if curr_node.prev:
+            curr_node.prev.next = curr_node.next
+        else:
+            # first node - update head
+            self.__head = curr_node.next
+
+        if curr_node.next:
+            curr_node.next.prev = curr_node.prev
+
         self.__total_points -= 1
 
     def __setitem__(self, ind, new_pt):
@@ -82,7 +102,12 @@ class Polygon:
             raise PolygonIndexError(ind)
 
     def __getitem__(self, ind):
-        return self.__points[ind]
+        # TODO - i'm not handling errors well
+        # TODO - i'm not handling negative values
+        curr_node = self.__head
+        for _ in range(ind):
+            curr_node = curr_node.next
+        return curr_node.pt
 
     def __len__(self):
         return self.__total_points
@@ -97,14 +122,54 @@ p.insert((0, 0), 0)
 p.insert((5, 2), 1)
 p.insert((10, 8), 2)
 p.insert((20, 2), 3)
+print(f'polygon contains {len(p)} points')
+
 p.insert((5, 8), 0)
 p.insert((7, 14), 0)
 p.insert((8, 88), 6)
 p.insert((8, 9), 7)
 p[0] = (20, 14)
+p[1] = (8, 8)
+p[7] = (12, 12)
 for pt in p:
     print(pt)
 
-# print(a[1])
-# print(a[0])
-# print(a[100])
+print(f'polygon contains {len(p)} points')
+print(f'Point at index 0 is {p[0]}')
+print(f'Point at index 3 is {p[3]}')
+
+print(f'Last Point is {p[-1]}')  # this is not working atm
+print(f'Last Point is {p[len(p) -1]}')
+
+print("removing point at index 0")
+p.remove(0)
+for pt in p:
+    print(pt)
+
+print("removing point at index 0")
+p.remove(0)
+for pt in p:
+    print(pt)
+
+print("removing point at index 1")
+p.remove(1)
+for pt in p:
+    print(pt)
+
+print("removing point at index 3")
+p.remove(3)
+for pt in p:
+    print(pt)
+
+print("removing point at index 2")
+p.remove(2)
+p.remove(10)
+for pt in p:
+    print(pt)
+
+p.remove(0)
+p.remove(0)
+p.remove(0)
+p.remove(0)
+for pt in p:
+    print(pt)
